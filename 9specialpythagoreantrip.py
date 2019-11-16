91b4ead942a411ad5b2e55d75e13e0dd85f8edc0
Manish Jung Thapa <manishjt@users.noreply.github.com>
2019-11-16 22:23:46 +0600

    Update 9specialpythagoreantrip.py

--- 9specialpythagoreantrip.py
+++ 9specialpythagoreantrip.py
@@ -20,16 +20,3 @@ for ai in a:
 			print ai,bi,ci
 			break
 
-			
-
-#From Forum
-#!/usr/bin/python 
-
-#c=0
-#while True:
-#	for b in range(0,c):
-#		for a in range(0,b):
-#			if a**2 + b**2 == c**2:
-#				if a+b+c == 1000:
-#					print "a=%d, b=%d, c=%d a*b*c=%d" % (a,b,c,a*b*c)
-#	c+=1
